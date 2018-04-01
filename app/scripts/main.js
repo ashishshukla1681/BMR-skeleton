7f4621b736bbb1378e1f80cc1f9bd593aa24682d
Ashish Kr Shukla <ashish.shukla@lexisnexis.com>
2018-03-31 23:35:39 -0400

    intial files/folders are created

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+require.config({
+    paths: {
+        'jquery': 'vendor/jquery/dist/jquery',
+        'underscore': 'vendor/underscore/underscore',
+        'backbone': 'vendor/backbone/backbone',
+    },
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            exports: 'Backbone',
+            deps: ['jquery', 'underscore']
+        }
+    },
+    deps: ['jquery', 'underscore']
+});
+
+require(['views/app'], function (AppView) {
+    new AppView;
+});
